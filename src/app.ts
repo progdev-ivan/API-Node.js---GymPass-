7e896d65bf690820c55cd68e43e84713eb62d7d5
ivan-leonardi <progdev.ivan@gmail.com>
2026-09-11 00:36:36 -0300

    feat: add prisma instance and route register

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,8 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { register } from './http/controllers/register';
+import { appRoutes } from './http/routes';
 
 export const app = fastify();
 
-export const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-  },
-});
+app.register(appRoutes);
