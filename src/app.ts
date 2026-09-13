8a2e46730248c6084c6931a88ea9e48b6fc7739e
ivan-leonardi <progdev.ivan@gmail.com>
2026-09-12 22:22:08 -0300

    feat: add bcryptjs to hash password

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import fastify from 'fastify';
-import { register } from './http/controllers/register';
 import { appRoutes } from './http/routes';
 
 export const app = fastify();
-
 
 app.register(appRoutes);
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
+import { hash } from 'bcryptjs';
 
 import { prisma } from '@/lib/prisma';
 
@@ -12,13 +13,27 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 8);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: 'User already exists.',
+    });
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   });
 
   return reply.status(201).send();
-};
+}
